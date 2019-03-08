d6132c2950c38344112f93be03d8f7196faf9775
harshith <jhonnybravo1234789@gmail.com>
2019-03-08 16:37:34 -0600

    added app.get

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const hostname = '0.0.0.0'    // allows access from remote computers
 const port = 3002
 
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+  res.send('Welcome users'.fontcolor("green").fontsize(1000).link("https://www.crictracker.com/wp-content/uploads/2018/07/Rohit-Sharma-3.jpg"))
 })
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello this is Harshith'.fontcolor("blue").fontsize(1000).link("https://dg.imgix.net/should-i-ever-take-an-action-i-don-t-have-peace-about-pcdtderl-en/landscape/should-i-ever-take-an-action-i-don-t-have-peace-about-pcdtderl-6d9ea9b7b052cbdeca79c26c7eff0fd5.jpg?ts=1536621334&ixlib=rails-3.0.2&auto=format%2Ccompress&fit=min&w=700&h=394&dpr=2&ch=Width%2CDPR"))
 })
 
 app.get('/big',  (req, res) =>{
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Enjoy the snow fall in missouiri</h1>'.fontcolor("grey").fontsize(1000).link("https://bloximages.chicago2.vip.townnews.com/maryvilledailyforum.com/content/tncms/assets/v3/editorial/0/d2/0d2d76b0-8d34-11e5-b77f-13b3697a2530/564b34038e14f.image.jpg?resize=1200%2C800"))
 })
 
 app.get('/greeting/:id',  (req, res) =>{
